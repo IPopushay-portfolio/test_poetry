ffd6a6714b242f90052949ecec6ace82f329ea38
Your Name <you@example.com>
2024-09-14 22:42:45 +0300

    init

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,10 @@
-def mask_account_card(card_number:[str]) -> [str]:
+def mask_account_card(card_number: [str]) -> [str]:
     """Функция принимает на вход номер карты и возвращает ее маску"""
 
-    return f'{card_number[:-5]}'
+    return f"{card_number[:-5]}"
 
 
-def get_date(account_number:[str]) -> [str]:
+def get_date(account_number: [str]) -> [str]:
     """Функция принимает на вход номер счета и возвращает его маску"""
 
-    return f'{account_number[-4:]}'
-
-
+    return f"{account_number[-4:]}"
--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,10 @@
 import datetime
 import masks
+
 """Импортируем библиотеку datetime"""
 
 
-def mask_account_card(type_num_account:[str]) -> [str]:
+def mask_account_card(type_num_account: [str]) -> [str]:
     """Функция умеет обрабатывать информацию как о картах, так и о счетах"""
     card_number = "".join(el if el.isdigit() else "" for el in type_num_account)
     number_card_mask = mask_account_card(card_number)
@@ -12,11 +13,9 @@ def mask_account_card(type_num_account:[str]) -> [str]:
     return data_card_mask
 
 
-
-def get_date(day_pay:[str]) -> [str]:
+def get_date(day_pay: [str]) -> [str]:
     """Функция принимает на вход строку и отдает корректный результат в формате
-11.07.2018"""
+    11.07.2018"""
     date_format = day_pay.strptime(day_pay, "%Y-%m-%dT%H:%M:%S.%f")
     new_date = date_format.strftime("%d.%m.%Y")
     return new_date
-
