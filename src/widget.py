c939b19be49f5885194db213924f471e44b5719b
Your Name <you@example.com>
2024-09-16 21:32:42 +0300

    add errors

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,28 @@
 import datetime
+from typing import Union
 import masks
 """Импортируем библиотеку datetime"""
 
-
 def mask_account_card(type_num_account: [str]) -> [str]:
     """Функция умеет обрабатывать информацию как о картах, так и о счетах"""
-    card_number = "".join(el if el.isdigit() else "" for el in type_num_account)
-    number_card_mask = mask_account_card(card_number)
-    name_card = "".join("" if el.isdigit() else el for el in type_num_account)
-    data_card_mask = name_card + number_card_mask
-    return data_card_mask
+    if "Счет" in type_num_account:
+        account = type_num_account[-22:]
+        return type_num_account[:4] + masks.get_mask_account(account)
+    else:
+        number_card = "".join(type_num_account[-16:].split())
+        return type_num_account[:-16] + masks.get_mask_card_number(number_card)
 
 
 def get_date(day_pay: [str]) -> [str]:
-    """Функция принимает на вход строку и отдает корректный результат в формате
-    11.07.2018"""
-    date_format = day_pay.strptime(day_pay, "%Y-%m-%dT%H:%M:%S.%f")
-    new_date = date_format.strftime("%d.%m.%Y")
-    return new_date
+    """Функция принимает на вход строку с датой в формате  и возвращает строку с датой"""
+    return f'{day_pay[8:10]}.{day_pay[5:7]}.{day_pay[:4]}'
+
+
+#print(mask_account_card('Visa Super Card 1234567890123456')) # выведет "Visa Super Card 1234 56** **** 3456"
+#print(mask_account_card('Maestro 1234578484648788')) # выведет "Maestro 1234 57** **** 8788"
+
+#print(mask_account_card('Счет 12345678901234567890')) # выведет "Счет **7890"
+#print(mask_account_card('Счет 13579064547897898745')) # выведет "Счет **8745"
+
+#print(get_date('2018-07-11T02:26:18.671407')) # выведет "11.07.2018"
+#print(get_date('2024-09-12T23:23:23.671407')) # выведет "12.09.2024"
