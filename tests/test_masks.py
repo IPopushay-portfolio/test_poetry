8ad688b5fa805b4fc4577deae7bca6d5c568be91
Your Name <you@example.com>
2024-09-21 22:54:45 +0300

    add linters

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,17 +2,23 @@ import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
 
-@pytest.mark.parametrize('card_number, mask_number',
-                         [('1234567890123456', '1234 56** **** 3456'),
-                             ('1234578484648788', '1234 57** **** 8788'), ])
-
-
+@pytest.mark.parametrize(
+    "card_number, mask_number",
+    [
+        ("1234567890123456", "1234 56** **** 3456"),
+        ("1234578484648788", "1234 57** **** 8788"),
+    ],
+)
 def get_mask_card_number(card_number, mask_number):
-    assert (get_mask_card_number(card_number) == mask_number)
+    assert get_mask_card_number(card_number) == mask_number
 
 
-@pytest.mark.parametrize('account_number, mask_number',
-                         [('12345678901234567890', ' **7890'), ('13579064547897898745', ' **8745'), ])
-
+@pytest.mark.parametrize(
+    "account_number, mask_number",
+    [
+        ("12345678901234567890", " **7890"),
+        ("13579064547897898745", " **8745"),
+    ],
+)
 def get_mask_account(account_number, mask_number):
-    assert (get_mask_account(account_number) == mask_number)
+    assert get_mask_account(account_number) == mask_number
